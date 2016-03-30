bbc456b18a4252e811d2e65b0d3187013522cb85
hylzml <642638112@qq.com>
2016-03-30 15:38:28 +0800

    增加动态功能

--- ChatLisview/circletime/src/main/java/com/circletime/CircleTime.java
+++ ChatLisview/circletime/src/main/java/com/circletime/CircleTime.java
@@ -10,8 +10,15 @@ import android.view.View;
  * Created by zml on 2016/3/29.
  */
 public class CircleTime extends View {
-    private  int width;
-    private  int high;
+    private int width;
+    private int high;
+
+    private int hourx;
+    private int houry;
+    private int minutx;
+    private int minuty;
+    private int miaox;
+    private int miaoy;
 
     public CircleTime(Context context) {
         super(context);
@@ -25,54 +32,159 @@ public class CircleTime extends View {
     public CircleTime(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
     }
+
     @Override
-       protected void onDraw(Canvas canvas) {
+    protected void onDraw(Canvas canvas) {
 
-        width=getMeasuredWidth();
-        high=getMeasuredHeight();
+        width = getMeasuredWidth();
+        high = getMeasuredHeight();
 
-        Paint circlePaint=new Paint();
+        Paint circlePaint = new Paint();
         circlePaint.setColor(getResources().getColor(R.color.colorAccent));
         circlePaint.setStrokeWidth(5);
         circlePaint.setStyle(Paint.Style.STROKE);
         circlePaint.setAntiAlias(true);
         canvas.drawCircle(width / 2, high / 2, width / 2, circlePaint);
 
-        Paint paintDegree=new Paint();
+        Paint paintDegree = new Paint();
         paintDegree.setStrokeWidth(3);
-        for(int i=0;i<24;i++){
-            if(i==0||i==6||i==12||i==18){
+        for (int i = 0; i < 12; i++) {
+            if (i == 0 || i == 3 || i == 6 || i == 12) {
                 paintDegree.setStrokeWidth(5);
                 paintDegree.setTextSize(30);
                 canvas.drawLine(width / 2, high / 2 - width / 2, width / 2, high / 2 - width / 2 + 60, paintDegree);
-                String degree=String.valueOf(i);
-                canvas.drawText(degree,width / 2, high / 2 - width / 2 + 90,paintDegree);
-            }
-            else {
+                String degree = String.valueOf(i);
+                canvas.drawText(degree, width / 2, high / 2 - width / 2 + 90, paintDegree);
+            } else {
                 paintDegree.setStrokeWidth(3);
                 paintDegree.setTextSize(15);
                 canvas.drawLine(width / 2, high / 2 - width / 2, width / 2, high / 2 - width / 2 + 30, paintDegree);
-                String degree=String.valueOf(i);
+                String degree = String.valueOf(i);
                 canvas.drawText(degree, width / 2, high / 2 - width / 2 + 60, paintDegree);
             }
-            canvas.rotate(15,width/2,high/2);
-
-
+            canvas.rotate(30, width / 2, high / 2);
 
 
         }
         Paint paintPointer = new Paint();
         paintPointer.setStrokeWidth(30);
         canvas.drawPoint(width / 2, high / 2, paintPointer);
-        // 鐢绘寚閽?        Paint paintHour = new Paint();
+
         paintPointer.setStrokeWidth(20);
+
+        Paint paintHour= new Paint();
+        paintHour.setStrokeWidth(10);
+
         Paint paintMinute = new Paint();
         paintMinute.setStrokeWidth(10);
+
+        Paint paintMiao = new Paint();
+        paintMiao.setStrokeWidth(10);
+
         canvas.save();
         canvas.translate(width / 2, high / 2);
-        canvas.drawLine(0, 0, 100, 100, paintMinute);
-        canvas.drawLine(0, 0, 100, 200, paintPointer);
+        canvas.drawLine(0, 0, hourx, houry, paintHour);
+        canvas.drawLine(0, 0, minutx, minuty, paintMinute);
+        canvas.drawLine(0, 0, miaox, miaoy, paintMiao);
         canvas.restore();
         super.onDraw(canvas);
     }
-}
+
+    public void setTime(int hour, int minute, int miao) {
+        System.out.println(hour + "--"+minute+"--"+miao);
+        if (hour >= 0 && hour <= 3) {
+            hourx = (int) (100 * Math.sin(Math.PI *hour / 6.0));
+            houry = -(int) (100 * Math.cos(Math.PI *hour / 6.0));
+
+        }
+        if (hour >= 3 && hour <= 6) {
+            System.out.println((((hour + 0.0) / 12) * 360 - 90) + "-------------------------------------");
+            double hx = (((hour + 0.0) / 12) * 360 - 90);
+
+            hourx = (int) (100 * Math.cos(Math.PI * (hx / 180.0)));
+            houry = (int) (100 * Math.sin(Math.PI * (hx / 180.0)));
+        }
+
+        if (hour >= 6 && hour <= 9) {
+            double hx = (((hour + 0.0) / 12) * 360 - 180);
+
+            hourx = -(int) (100 * Math.sin(Math.PI * (hx / 180.0)));
+            houry = (int) (100 * Math.cos(Math.PI * (hx / 180.0)));
+        }
+        if (hour >= 9 && hour <= 12) {
+            double hx = (((hour + 0.0) / 12) * 360 - 270);
+
+            hourx = -(int) (100 * Math.cos(Math.PI * (hx / 180.0)));
+            houry = -(int) (100 * Math.sin(Math.PI * (hx / 180.0)));
+        }
+//分
+        if (minute >= 0 && minute <= 15) {
+            double hx = (((minute + 0.0) / 60.0) * 360 );
+            System.out.println("minute" +hx+"%%%%%%%%%%%%%%%%%%%%%%%%%%");
+            minutx = (int) (200 * Math.sin(Math.PI * (hx / 180.0)));
+
+            minuty = -(int) (200 * Math.cos(Math.PI * (hx / 180.0)));
+            System.out.println("minute" +minutx+"%%%%%%%%%%%%%%%%%%%%%%%%%%");
+            System.out.println("minute" +minuty+"%%%%%%%%%%%%%%%%%%%%%%%%%%");
+        }
+        if (minute >= 15 && minute <= 30) {
+
+            double hx = (((minute + 0.0) / 60.0) * 360 - 90);
+            minutx = (int) (200 * Math.cos(Math.PI * (hx / 180.0)));
+            minuty = (int) (200 * Math.sin(Math.PI * (hx / 180.0)));
+
+        }
+
+        if (minute >= 30 && minute <= 45) {
+            System.out.println("minute" + (((minute + 0.0) / 60) * 360 - 180) + "-------------------------------------");
+            double hx = (((minute + 0.0) / 60.0) * 360 - 180);
+            System.out.println("minute" + hx + "-------------------------------------");
+            minutx = -(int) (200 * Math.sin(Math.PI * (hx / 180.0)));
+            minuty = (int) (200 * Math.cos(Math.PI * (hx / 180.0)));
+
+        }
+        if (minute >= 45 && minute <= 60) {
+            double hx = (((minute + 0.0) / 60.0) * 360 - 270);
+
+            minutx = -(int) (200 * Math.cos(Math.PI * (hx / 180.0)));
+            minuty = -(int) (200 * Math.sin(Math.PI * (hx / 180.0)));
+        }
+
+        //秒
+        if (miao >= 0 && miao <= 15) {
+            double hx = (((miao + 0.0) / 60.0) * 360 );
+            miaox = (int) (270 * Math.sin(Math.PI * (hx / 180.0)));
+
+
+            miaoy = -(int) (270 * Math.cos(Math.PI * (hx / 180.0)));
+
+            System.out.println("miaox" +miaox+"%%%%%%%%%%%%%%%%%%%%%%%%%%");
+            System.out.println("miaoy" +miaoy+"%%%%%%%%%%%%%%%%%%%%%%%%%%");
+        }
+        if (miao >= 15 && miao <= 30) {
+
+            double hx = (((miao + 0.0) / 60.0) * 360 - 90);
+
+            miaox = (int) (270 * Math.cos(Math.PI * (hx / 180.0)));
+            miaoy = (int) (270 * Math.sin(Math.PI * (hx / 180.0)));
+
+        }
+            if (miao >= 30 && miao <= 45) {
+
+                double hx = (((miao + 0.0) / 60.0) * 360 - 180);
+                System.out.println("minute" + hx + "-------------------------------------");
+                miaox = -(int) (270 * Math.sin(Math.PI * (hx / 180.0)));
+                miaoy = (int) (270 * Math.cos(Math.PI * (hx / 180.0)));
+                System.out.println("minute" + minutx + "-------------------------------------" + minuty);
+            }
+            if (miao >= 45 && miao <= 60) {
+                double hx = (((miao + 0.0) / 60.0) * 360 - 270);
+
+                miaox = -(int) (270 * Math.cos(Math.PI * (hx / 180.0)));
+                miaoy = -(int) (270 * Math.sin(Math.PI * (hx / 180.0)));
+            }
+
+
+            this.invalidate();
+        }
+    }
--- ChatLisview/circletime/src/main/java/com/circletime/MainActivity.java
+++ ChatLisview/circletime/src/main/java/com/circletime/MainActivity.java
@@ -1,15 +1,76 @@
 package com.circletime;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import java.util.Calendar;
+
 public class MainActivity extends AppCompatActivity {
+private  CircleTime circleTime;
+    int   miao;
+    int hour;
+    int minute;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        intview();
+    }
 
+    private void intview() {
+
+
+
+        circleTime=(CircleTime)findViewById(R.id.circle);
+      //  circleTime.setTime(hour, minute, miao);
+
+        new ProgressAnimation().execute();
+    }
+
+    class ProgressAnimation extends AsyncTask<Void, String, Void> {
+        @Override
+        protected Void doInBackground(Void... params) {
+
+
+
+
+
+            for (int i=1;i<10;i=1) {
+                Calendar c1=Calendar.getInstance();
+                hour = c1.get(Calendar.HOUR_OF_DAY);
+                if(hour>12){hour=hour-12;}
+                minute = c1.get(Calendar.MINUTE);
+                miao = c1.get(Calendar.SECOND);
+                try {
+
+                    System.out.println(minute + "-------------------for");
+
+                    publishProgress(miao+"/"+minute+"/"+hour);
+                    Thread.sleep(1000);
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(String... values) {
+            String[] array= new String[3];
+            array=values[0].split("/");
+
+          int miao= Integer.parseInt(array[0]);
+           int fen= Integer.parseInt(array[1]);
+          int xiaoshi= Integer.parseInt(array[2]);
+       circleTime.setTime(xiaoshi,fen,miao);
+
+      System.out.println(array[0] + "-------------------");
+            super.onProgressUpdate(values);
+        }
     }
 
 
